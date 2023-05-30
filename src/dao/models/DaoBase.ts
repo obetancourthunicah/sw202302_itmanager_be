f11454d21d1db323dcb57e597491df2e5aa6fa80
Orlando J Betancourth <orlando.betancourth@gmail.com>
2023-05-29 19:00:01 -0600

    Using Abstract for Dao Model

---
+++ src/dao/models/DaoBase.ts
@@ -0,0 +1,7 @@
+export abstract class DaoBase<T> {
+  public abstract create(item: T): Promise<T>;
+  public abstract update(id: string, item: Partial<T>): Promise<T>;
+  public abstract delete(id: string): Promise<boolean>;
+  public abstract find(item: Partial<T>): Promise<T[]>;
+  public abstract findOne(id: string): Promise<T>;
+}
--- src/libs/projects/projects.ts
+++ src/libs/projects/projects.ts
@@ -1,3 +1,4 @@
+import { ProjectDao } from '@dao/models/ProjectsDao';
 export interface IProject {
   _id?: string;
   name: string;
@@ -7,39 +8,24 @@ export interface IProject {
   updatedAt?: Date;
 }
 
-// const newProject: Required<IProject> = {};
-const memoryProjects: IProject[] = [];
-let createdProjects: number = 0;
+const ProjectDaoInstance = new ProjectDao();
 
-export const createProject = async (project: IProject) => {
-  const newProject = { ...project };
-  newProject._id = (++createdProjects).toString();
-  newProject.createdAt = new Date();
-  newProject.updatedAt = newProject.createdAt;
-  memoryProjects.push(newProject);
-  return newProject;
+export const createProject = (project: IProject) => {
+  return ProjectDaoInstance.create(project);
 }
 
-export const getProjects = async () => {
-  return memoryProjects;
+export const getProjects = () => {
+  return ProjectDaoInstance.find({});
 };
 
-export const getProject = async (id:string) => {
-  const project = memoryProjects.find(p => p._id === id);
-  if (!project) throw new Error('Project not found');
-  return project;
+export const getProject = (id:string) => {
+  return ProjectDaoInstance.findOne(id);
 }
 
 export const updateProject = ( id:string, project:Partial<IProject>) => {
-  const index = memoryProjects.findIndex(p => p._id === id);
-  if (index === -1) throw new Error('Project not found');
-  memoryProjects[index] = { ...memoryProjects[index], ...project, updatedAt: new Date() };
-  return memoryProjects[index];
+  return ProjectDaoInstance.update(id, project);
 }
 
 export const deleteProject = (id:string) => {
-  const index = memoryProjects.findIndex(p => p._id === id);
-  if (index === -1) throw new Error('Project not found');
-  memoryProjects.splice(index, 1);
-  return true;
+  return ProjectDaoInstance.delete(id);
 }
